4e86732a15b725052441bbec98a72c685c039b48
sknadim1993 <smnadim.gov@gmail.com>
2025-11-09 15:39:05 +0530

    Update entryValidators.ts

--- src/validators/entryValidators.ts
+++ src/validators/entryValidators.ts
@@ -84,6 +84,18 @@ export const paginationSchema = z.object({
 
 export const searchSchema = z.object({
   title: z.string().min(1, 'Search query is required'),
+  page: z
+    .string()
+    .optional()
+    .default('1')
+    .transform((val) => parseInt(val, 10))
+    .refine((val) => val > 0, 'Page must be greater than 0'),
+  limit: z
+    .string()
+    .optional()
+    .default('10')
+    .transform((val) => parseInt(val, 10))
+    .refine((val) => val > 0 && val <= 100, 'Limit must be between 1 and 100'),
 });
 
 export type CreateEntryInput = z.infer<typeof createEntrySchema>;
